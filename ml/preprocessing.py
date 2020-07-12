cb71ea45a16766c4bbf69c932faf5e331902e84d
Ashish Subedi <ashishsubedi10@outlook.com>
2020-07-12 11:29:56 +0545

    Warps images

---
+++ ml/preprocessing.py
@@ -0,0 +1,60 @@
+import os
+import cv2
+import numpy as np
+import imutils
+from helpers.transform import four_point_transform
+
+ratio = None
+
+
+def load_image(imgPath):
+    if not os.path.exists(imgPath):
+        return None, None
+    global ratio
+    image = cv2.imread(imgPath)
+    ratio = image.shape[0]/500.0
+    orig = image.copy()
+    image = imutils.resize(image, height=500)
+    return image, orig
+
+
+def wrap_transform(image, orig):
+    global ratio
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 1)
+    edged = cv2.Canny(gray, 75, 200)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:3]
+    screenCnt = None
+    for i, c in enumerate(cnts):
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02*peri, True)
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+    if screenCnt is not None:
+        warped = four_point_transform(orig, screenCnt.reshape(4, 2)*ratio)
+        warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+        T = cv2.adaptiveThreshold(
+            warped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 10)
+        return T, warped
+    else:
+        return None, None
+
+
+if __name__ == "__main__":
+    resized, original = load_image(
+        'C:\\Users\\Dell\\Desktop\\nutri.gram\\ml\\images\\labels_2_cropped.jpg')
+
+    if(resized is not None):
+        T, warped = wrap_transform(resized, original)
+        if T is not None:
+            cv2.imshow("Threshold", T)
+            cv2.imshow("Warped", imutils.resize(warped, height=500))
+            cv2.waitKey(0)
+        else:
+            print("T is none")
+    else:
+        print("Image doesnot exist")
