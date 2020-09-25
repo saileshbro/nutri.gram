3193bbcd92943fdb3b87e110e85d00b59d63a4cf
Sailesh Dahal <saileshbro@gmail.com>
2020-09-25 17:05:10 +0545

    updated. added scan route

--- api/controllers/scan.controller.js
+++ api/controllers/scan.controller.js
@@ -1,13 +1,27 @@
 const { default: Axios } = require("axios")
 const FormData = require("form-data")
 const CustomError = require("../handlers/custom_error")
-
+/**
+ * scans image fetches image content from ML server at :5000 port
+ * @param {Request} req
+ * @param {Response} res
+ */
 exports.scanImage = async (req, res) => {
   if (!req.file) {
     throw new CustomError(400, "Please provide an image!")
   }
-  const { fieldName, buffer } = req.file
-  const formData = new FormData()
-  formData.append(fieldName, buffer)
-  await Axios.post("url", formData, { headers: formData.getHeaders() })
+  const bufferObject = Buffer.from(req.file.buffer)
+  const file = new FormData()
+  file.append("image", bufferObject, `image.${req.file.mimetype.split("/")[1]}`)
+  const fullUrl = `${req.protocol}://${req.host}`
+  console.log(fullUrl)
+
+  const resp = await Axios.post(`${fullUrl}:5000/upload`, file, { headers: file.getHeaders() }).catch(() => {
+    throw new CustomError(500, "Error while fetching data from image scanning server!")
+  })
+  if (res.status >= 400) {
+    throw new CustomError(500, "Error while fetching data from image scanning server!")
+  }
+  const { data } = resp.data
+  return res.json({ data })
 }
